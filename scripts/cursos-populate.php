0e2900729232e306a1731d3186296112b1ae3fc7
duhow <duhowpi@gmail.com>
2017-05-08 20:25:19 +0200

    WIP #3: Creación de cursos.

--- scripts/cursos-populate.php
+++ scripts/cursos-populate.php
@@ -15,12 +15,12 @@ $moodle->type("rest");
 
 $plantilla = json_decode(file_get_contents("courses.json"), TRUE);
 
-foreach($plantilla as $curso){
+foreach($plantilla as $cat){
     // Crear categoría
     $data = [
-        "idnumber"      => $curso["id"],
-        "name"          => $curso["name"],
-        "description"   => $curso["name"],
+        "idnumber"      => $cat["id"],
+        "name"          => $cat["name"],
+        "description"   => $cat["name"],
         "parent"        => "0", // Sin padre
     ];
 
@@ -29,12 +29,49 @@ foreach($plantilla as $curso){
     $res = $moodle->query("core_course_create_categories", $category);
 
     if(isset($res->exception)){
-        error_log("Error al generar la categoría " .$curso["id"] .": " .$res->message);
+        if($res->errorcode == "categoryidnumbertaken"){
+            error_log("Categoría " .$cat["id"] ." duplicada, omitiendo.");
+            continue;
+        }
+
+        error_log("Error al generar la categoría " .$cat["id"] .": " .$res->message);
         exit(-1);
     }
 
     $catid = $res[0]->id;
-    echo "Categoría " .$curso["id"] ." creado: $catid\n";
+    echo "Categoría " .$cat["id"] ." creado: $catid\n";
+
+    if(!isset($cat["courses"])){ continue; }
+    foreach($cat["courses"] as $id => $curso){
+        // Crear curso
+        $curid = $cat["id"] ."-" .$id;
+
+        $sections = $curso;
+        unset($sections["id"]);
+        unset($sections["name"]);
+
+        $data = [
+            "fullname" => $curso["name"],
+            "shortname" => $curid,
+            "categoryid" => $catid,
+            "visible" => 1, // TRUE
+            "idnumber" => $curid,
+            "format" => "topics", // Formato de temas
+            "numsections" => count($sections),
+        ];
+
+        $course = ["courses" => [0 => $data]];
+
+        $res = $moodle->query("core_course_create_courses", $course);
+
+        if(isset($res->exception)){
+            error_log("Error al generar el curso " .$id .": " .$res->message);
+            exit(-1);
+        }
+
+        $courseid = $res[0]->id;
+        echo "Curso $courseid creado: " .$curso["name"] ."\n";
+    }
 }
 
 ?>
