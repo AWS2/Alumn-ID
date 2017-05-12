fba592bbe47a779929faa0471e679086b107150e
duhow <duhowpi@gmail.com>
2017-05-12 16:52:36 +0200

    Cambiar ID de rol por defecto.

--- scripts/config.php
+++ scripts/config.php
@@ -3,8 +3,8 @@
 return [
     'url' => "",
     'token' => "",
-    'student' => 1, // ID de rol de student.
-    'teacher' => 5, // ID de rol de profe.
+    'student' => 5, // ID de rol de student.
+    'teacher' => 3, // ID de rol de profe.
     'from' => "2017-09-21", // Fecha inicio matricula
     'to' => "2018-06-25", // Fecha final matricula
 ];
