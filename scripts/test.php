0f4ffbc9812720c6c500d0813e4f7bf887ee4c42
duhow <contacto@duhowpi.net>
2017-05-22 14:36:57 +0200

    Script para comprobar funciones.

--- scripts/test.php
+++ scripts/test.php
@@ -14,6 +14,33 @@ $moodle->type("rest");
 
 $res = $moodle->query("core_webservice_get_site_info");
 
-var_dump($res);
+if(isset($res->exception) and $res->exception == "webservice_access_exception"){
+	echo "No tienes permiso para cargar esta información.\n"
+		."Agrega la siguiente función: core_webservice_get_site_info\n\n";
+	exit(-1);
+}
+
+$required = [
+	"core_user_get_users_by_field",
+	"core_user_create_users",
+	"core_user_get_users_by_field",
+	"enrol_manual_enrol_users",
+	"core_course_create_categories",
+	"core_course_create_courses",
+	"core_course_get_contents",
+	"core_update_inplace_editable",
+	"core_group_create_groups",
+	"core_group_add_group_members",
+];
+
+$funcs = array();
+foreach($res->functions as $f){ $funcs[] = $r->name; }
+
+foreach($required as $r){
+	$str = "- $r :";
+	echo str_pad($str, 40, " ", STR_PAD_RIGHT);
+	echo (in_array($r, $funcs) ? "OK" : "NO");
+	echo "\n";
+}
 
 ?>
